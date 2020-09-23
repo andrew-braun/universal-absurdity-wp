faa3a6bdc66cb6715f643610df782149ecd2cc5a
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-09-23 17:42:51 +0400

    feat: Make breadcrumbs dynamic and applied only to child pages

--- page.php
+++ page.php
@@ -4,7 +4,7 @@ while (have_posts()) {
     the_post(); ?>
 
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/images/ocean.jpg") ?>;" ></div>
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/images/ocean.jpg") ?>;"></div>
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title"><?php the_title(); ?></h1>
             <div class="page-banner__intro">
@@ -15,9 +15,21 @@ while (have_posts()) {
 
     <div class="container container--narrow page-section">
 
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-        </div>
+    <?php 
+        $pageParent = wp_get_post_parent_id(get_the_ID());
+
+        if ( $pageParent ) { ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($pageParent) ?>">
+                        <i class="fa fa-home" aria-hidden="true"></i>
+                        Go back to <?php echo get_the_title($pageParent) ?>
+                    </a>
+                    <span class="metabox__main"><?php the_title() ?></span>
+                </p>
+            </div>
+        <?php }
+    ?>
 
         <!-- <div class="page-links">
             <h2 class="page-links__title"><a href="#">About Us</a></h2>
