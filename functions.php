86fb32aed12020c16dffce807d5ab8f1a77deafd
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-09-29 22:36:32 +0400

    fix: Add style.css back to maintain page styles without Node

--- functions.php
+++ functions.php
@@ -3,8 +3,14 @@
 function universal_imports() {
     wp_enqueue_style("google-fonts", "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
     wp_enqueue_style("font-awesome", "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
-    wp_enqueue_style("universal_styles", get_stylesheet_uri());
-    wp_enqueue_script("universal-js", get_theme_file_uri("/js/scripts-bundled.js"), NULL, "1.0", true);
+    if (strstr($_SERVER["SERVER_NAME"], "universal-absurdity.local")) {
+        wp_enqueue_script("universal-js", "http://localhost:3000/bundled.js", NULL, "1.0", true);
+        wp_enqueue_style("our-main-styles", get_theme_file_uri("/bundled-assets/styles.5270ce0561d57b985c13.css"));
+    } else {
+        wp_enqueue_script("our-vendors-js", get_theme_file_uri("/bundled-assets/vendors~scripts.8c97d901916ad616a264.js"), NULL, "1.0", true);
+        wp_enqueue_script("universal-js", get_theme_file_uri("/bundled-assets/scripts.5270ce0561d57b985c13.js"), NULL, "1.0", true);
+        wp_enqueue_style("our-main-styles", get_theme_file_uri("/bundled-assets/styles.5270ce0561d57b985c13.css"));
+    }
 }
 
 add_action("wp_enqueue_scripts", "universal_imports");
