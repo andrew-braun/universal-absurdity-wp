68ec8e399766bdeab2e38abd90fc7a7b23da4f08
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-10-01 14:58:02 +0400

    feat: Create separate permalinks/styles for events pages

--- front-page.php
+++ front-page.php
@@ -14,11 +14,32 @@
     <div class="full-width-split__one">
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+            <?php 
+                $homepageEvents = new WP_Query(array(
+                    "posts_per_page" => 2,
+                    "post_type" => "event"
+                ));
+                while($homepageEvents->have_posts()) {
+                    $homepageEvents->the_post();
+                    ?>
+                    <div class="event-summary">
+                        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                            <span class="event-summary__month"><?php the_time("M"); ?></span>
+                            <span class="event-summary__day"><?php the_time("d"); ?></span>
+                        </a>
+                        <div class="event-summary__content">
+                            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                            <p><?php echo wp_trim_words(get_the_content(), 28); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                        </div>
+                    </div>
 
-            <div class="event-summary">
+
+            <?php    }
+            ?>
+            <!-- <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Mar</span>
-                    <span class="event-summary__day">25</span>
+                    <span class="event-summary__month"></span>
+                    <span class="event-summary__day"></span>
                 </a>
                 <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
@@ -34,7 +55,7 @@
                     <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
                     <p>Live experimental music, a mystery taco truck, and more mildly upsetting entertainment can found at our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
                 </div>
-            </div>
+            </div> -->
 
             <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
         </div>
--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php 
 
+/* Import styles and scripts */
 function universal_imports() {
     wp_enqueue_style("google-fonts", "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
     wp_enqueue_style("font-awesome", "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
@@ -12,16 +13,17 @@ function universal_imports() {
         wp_enqueue_style("our-main-styles", get_theme_file_uri("/bundled-assets/styles.5270ce0561d57b985c13.css"));
     }
 }
-
+/* Execute the import function */
 add_action("wp_enqueue_scripts", "universal_imports");
 
+/* Initiate website features */
 function universal_features() {
     add_theme_support("title-tag");
     register_nav_menu("headerMenuLocation", "Header Menu Location");
     register_nav_menu("footerMenuLocation1", "Footer Menu Location 1");
     register_nav_menu("footerMenuLocation2", "Footer Menu Location 2");
 }
-
+/* Execute the add features action */
 add_action("after_setup_theme", "universal_features");
 
 ?>
