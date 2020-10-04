2002de2e13abc62005b534ca75086511a99b2d7f
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-10-04 16:46:46 +0400

    feat: Add Past Events page

--- archive-event.php
+++ archive-event.php
@@ -41,6 +41,12 @@
         <?php } 
             echo paginate_links();
         ?>
+        <hr class="section-break"/>
+        <div class="past-events-container">
+            <p>Check out what we've done in the past!
+                <a href="<?php echo site_url("/past-events"); ?>"><button>Past events</button></a>
+            </p>
+        </div>
 </div>
 
 <?php get_footer() ?>
